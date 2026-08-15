375dba26d6a2b49b38f88b409941d07b42e8b83c
Julie Sun <152749775+juliexsun@users.noreply.github.com>
2026-08-15 19:29:46 -0400

    Update message content in send.js

--- send.js
+++ send.js
@@ -4,6 +4,6 @@ fetch(webhook, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
-    content: "<@628051595883708446> rewatch aot"
+    content: "<@628051595883708446> rewatch aot\n<@&1514403533976240230> listen to https://open.spotify.com/playlist/6GzztqV8FWjn5l8Tf7ycmf?si=fIzcqfpiS02DZg0xtht0kA"
   })
 });
